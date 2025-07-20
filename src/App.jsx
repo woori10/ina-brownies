144e1cdfbf8d144ddb85b1ae763657519ed7cbc7
raina.disa <raina.disa@apps.ipb.ac.id>
2025-07-20 19:34:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">
+        Ini seharusnya merah
+      </h1>
+    </div>
+  )
+}
+
+export default App
