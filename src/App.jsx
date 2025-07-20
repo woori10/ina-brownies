92c8e6b92f1aba1494af087e88432804d94e9748
raina.disa <raina.disa@apps.ipb.ac.id>
2025-07-20 19:42:18 +0700

    pertama

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="min-h-screen bg-white flex items-center justify-center">
       <h1 className="text-4xl font-bold text-red-600">
-        Ini seharusnya merah
+        Hello World
       </h1>
     </div>
   )
